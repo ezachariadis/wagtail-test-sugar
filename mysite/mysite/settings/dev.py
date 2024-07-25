fd3a8162955d6ce8ce808d37c9aff9a9d80376b0
ezachariadis <39909872+ezachariadis@users.noreply.github.com>
2024-07-26 02:10:30 +0300

    First Commit

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-_(b(+!3+zs*_9=jx%5j)ykuq0++e%88th*t3oetpygi%2t+=19"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
